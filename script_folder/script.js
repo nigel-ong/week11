81243beb8263c6b013eabae66bf4cc25aa030885
Nigel <nigel.ong00@gmail.com>
2022-11-15 10:45:53 -0800

    added defer, added an array and used a for loop to iternate through it. and append to the listItemElement

--- script_folder/script.js
+++ script_folder/script.js
@@ -1,8 +1,8 @@
 // simple function
 
-function greet(somename){
-    return console.log(`Hello ${somename}!`);
-}
+// function greet(somename){
+//     return console.log(`Hello ${somename}!`);
+// }
 
 // variable as a function
 
@@ -17,9 +17,24 @@ function greet(somename){
 // }
 
 // how to add a default value to time, but if we pass a value it will be invoked
-function greet(somename, time = "Day"){
-    console.log(`Good ${time} ${somename}!`);
+// function greet(somename, time = "Day"){
+//     console.log(`Good ${time} ${somename}!`);
+// }
+
+// greet("Nigel");
+// greet("Neda");
+
+
+const unorderedListElement = document.querySelector(".shopping")
+function populateList(list){
+    for (let i = 0 ; i <list.length; i++){
+        console.log(list[i]); // for each of these items, create and li, append it to an unorderedlistElements
+        const foodlistElement = document.createElement("li")
+        foodlistElement.textContent = list[i];
+        unorderedListElement.appendChild(foodlistElement);
+    }
 }
 
-greet("Nigel");
-greet("Neda");
+let shippingList = ["cheese", "bread", "pepper"];
+
+populateList(shippingList);
