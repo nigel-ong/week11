fc6f7ab85474ff99e7ca1dda24d2d2c1d2ae3269
Nigel <nigel.ong00@gmail.com>
2022-11-15 09:54:23 -0800

    added default values

--- script_folder/script.js
+++ script_folder/script.js
@@ -1,17 +1,25 @@
 // simple function
-// function greet(somename){
-//     return console.log(`Hello ${somename}!`);
-// }
+
+function greet(somename){
+    return console.log(`Hello ${somename}!`);
+}
 
 // variable as a function
+
 // let greet = function (somename){
 //     return console.log(`Hello ${somename}!`);
 // }
 
 // arrow function
+
 // let greet = (somename) => {
 //     return console.log(`Hello ${somename}!`);
 // }
 
-// greet("Nigel");
-// greet("Neda");
+// how to add a default value to time, but if we pass a value it will be invoked
+function greet(somename, time = "Day"){
+    console.log(`Good ${time} ${somename}!`);
+}
+
+greet("Nigel");
+greet("Neda");
